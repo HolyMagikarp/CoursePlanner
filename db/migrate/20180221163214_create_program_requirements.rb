7ab543839a443ab8de46ab472026dc894b1b53bb
Danny Liu <danny.liu@flipp.com>
2018-02-21 12:26:57 -0500

    removing program requirement model

--- db/migrate/20180221163214_create_program_requirements.rb
+++
@@ -1,9 +0,0 @@
-class CreateProgramRequirements < ActiveRecord::Migration[5.1]
-  def change
-    create_table :program_requirements do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
