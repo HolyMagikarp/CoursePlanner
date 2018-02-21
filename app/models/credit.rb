13535d3decf35bab872c3da5e62d56d774c97d5f
Danny Liu <danny.liu@flipp.com>
2018-02-21 11:36:56 -0500

    adding schema.rb to git ignore and adding annotations

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -1,2 +1,11 @@
 class Credit < ApplicationRecord
 end
+
+# == Schema Information
+#
+# Table name: credits
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,2 +1,18 @@
 class Lecture < ApplicationRecord
 end
+
+# == Schema Information
+#
+# Table name: lectures
+#
+#  id           :integer          not null, primary key
+#  lecture_code :string(255)
+#  location     :string(255)
+#  professor    :string(255)
+#  day          :string(255)
+#  start_time   :time
+#  end_time     :time
+#  duration     :time
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
