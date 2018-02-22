6c3bbc7bb312910d9c5abbb4059b4a41c3007181
Derek Chau <derek.chau@wishabi.com>
2018-02-22 11:44:23 -0500

    YAY MERGED

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,22 @@
 class PagesController < ApplicationController
+
   def planner
-    puts session[:user_id]
-    flash.now.alert = 'nothing'
+    set_courses
+    puts @courses.count
   end
 
-  def course_listing
+  def _course_listing
     @courses = Course.for_display.order('course_code').paginate(page: params[:page],
                                                                 per_page: 50)
   end
 
   def program_requirements
     render "programs/program_requirements"
+  end
+
+  def set_courses
+    @courses = Course.for_display.order('course_code').paginate(page: params[:page],
+                                                                per_page: 50)
   end
 
 end
